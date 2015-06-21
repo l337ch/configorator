755d307b52273f9ff931bcc3d62039224719560b
Lee Chang <leetchang@gmail.com>
2015-06-21 13:17:33 -0700

    add port flag and remove expose on dockerfile "

--- web.go
+++ web.go
@@ -1,39 +1,40 @@
 package main
 
 import (
-  "net"
-  "net/http"
-  "text/template"
-  "path/filepath"
+	"net"
+	"net/http"
+	"path/filepath"
+	"text/template"
 )
 
 var baseConfig *template.Template
 
 type CloudConfig struct {
-  Config  string
-  Content interface{}
+	Config  string
+	Content interface{}
 }
 
 type HostConfig struct {
-  HostIpAddr string
+	HostIpAddr string
 }
 
 //type ipAddress string
 
 func renderConfig(w http.ResponseWriter, r *http.Request) {
-  ipAddr, _, _ := net.SplitHostPort(r.RemoteAddr)
-  var err error = baseConfig.Execute(w, HostConfig{
-    ipAddr,
-  })
-  if err != nil {
-    panic(err)
-  }
+	ipAddr, _, _ := net.SplitHostPort(r.RemoteAddr)
+	var err error = baseConfig.Execute(w, HostConfig{
+		ipAddr,
+	})
+	if err != nil {
+		panic(err)
+	}
 }
 
-func webListener(templateDir string) {
-  templatePath := templateDir + "/base-config.tmpl"
-  baseConfigTemplateFile, _ := filepath.Abs(templatePath) 
-  baseConfig = template.Must(template.ParseFiles(baseConfigTemplateFile))
-  http.HandleFunc("/cloud-configs/", renderConfig)
-  http.ListenAndServe(":80", nil)
+func webListener(templateDir string, listenPort string) {
+	templatePath := templateDir + "/base-config.tmpl"
+	port := ":" + listenPort
+	baseConfigTemplateFile, _ := filepath.Abs(templatePath)
+	baseConfig = template.Must(template.ParseFiles(baseConfigTemplateFile))
+	http.HandleFunc("/cloud-configs/", renderConfig)
+	http.ListenAndServe(port, nil)
 }
